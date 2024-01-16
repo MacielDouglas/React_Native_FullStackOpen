576dc7bfabe74522866809c07ecbf4994e045a4e
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-01-16 15:11:27 -0300

    terminado exercício 10.9

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,17 +14,29 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingLeft: 10,
   },
+  errorText: {
+    color: '#d73a4a',
+    marginBottom: 6,
+  },
+});
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required('Username is required'),
+  password: Yup.string().required('Password is required'),
 });
 
 export default function SignIn() {
   const onSubmit = (values) => {
-    // Aqui você pode lidar com os dados do formulário, como fazer uma solicitação de login
     console.log('Sign In:', values);
   };
 
   return (
-    <Formik initialValues={{ username: '', password: '' }} onSubmit={onSubmit}>
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
         <View style={styles.container}>
           <TextInput
             style={styles.input}
@@ -32,15 +45,21 @@ export default function SignIn() {
             value={values.username}
             placeholder="Username"
           />
+          {errors.username && (
+            <Text style={styles.errorText}>{errors.username}</Text>
+          )}
           <TextInput
             style={styles.input}
             onChangeText={handleChange('password')}
             onBlur={handleBlur('password')}
             value={values.password}
             placeholder="Password"
-            secureTextEntry={true}
+            secureTextEntry
           />
-          <Button onPress={handleSubmit} title="Sign In" />
+          {errors.password && (
+            <Text style={styles.errorText}>{errors.password}</Text>
+          )}
+          <Button onPress={handleSubmit} title="Login" />
         </View>
       )}
     </Formik>
