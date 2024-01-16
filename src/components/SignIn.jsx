b08ef81be8c5d6c55606533036b79af3015c80ac
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-01-16 13:59:51 -0300

    Terminado exercício 10.8, usando o Formik

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,48 @@
-import Text from './Text';
+import React from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
 
-const SignIn = () => {
-  return <Text>The sign-in view</Text>;
-};
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
+});
 
-export default SignIn;
+export default function SignIn() {
+  const onSubmit = (values) => {
+    // Aqui você pode lidar com os dados do formulário, como fazer uma solicitação de login
+    console.log('Sign In:', values);
+  };
+
+  return (
+    <Formik initialValues={{ username: '', password: '' }} onSubmit={onSubmit}>
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View style={styles.container}>
+          <TextInput
+            style={styles.input}
+            onChangeText={handleChange('username')}
+            onBlur={handleBlur('username')}
+            value={values.username}
+            placeholder="Username"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={handleChange('password')}
+            onBlur={handleBlur('password')}
+            value={values.password}
+            placeholder="Password"
+            secureTextEntry={true}
+          />
+          <Button onPress={handleSubmit} title="Sign In" />
+        </View>
+      )}
+    </Formik>
+  );
+}
